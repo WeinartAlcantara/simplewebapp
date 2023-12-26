d8b19951977eb5ffadf3d2bbc3bad362336ae23a
WeinartAlcantara <141305675+WeinartAlcantara@users.noreply.github.com>
2023-12-27 00:47:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 import os
+import random
 
 app = Flask(__name__, template_folder="templates")
 
-color = os.getenv("COLOR", "#FFFFFF")
+# Generate a random color
+random_color = "#{:06x}".format(random.randint(0, 0xFFFFFF))
+
+color = os.getenv("COLOR", random_color)
 
 @app.route("/")
-def hello():
-    return render_template("index.html", message="Hello from Flask!", "color":color)
+def index():
+    return render_template("index.html", message="Hello from Flask!", color=color)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5001)
