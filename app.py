5c3023a862d51d2be4c9231f3296401622b02b97
WeinartAlcantara <141305675+WeinartAlcantara@users.noreply.github.com>
2023-12-26 18:47:13 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify, os
+
+app = Flask(__name__)
+
+color = os.getenv("COLOR", "#FFFFFF")
+
+@app.route("/")
+def hello():
+    return jsonify({"message": "Hello from Flask!", "color": color})
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
