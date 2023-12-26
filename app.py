89e8b8c77e8f004520085cd1aecfccd63b3ab879
WeinartAlcantara <141305675+WeinartAlcantara@users.noreply.github.com>
2023-12-27 00:40:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ color = os.getenv("COLOR", "#FFFFFF")
 
 @app.route("/")
 def hello():
-    return render_template("index.html", message="Hello from Flask!", color=color)
+    return render_template("index.html", message="Hello from Flask!", "color":color)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5001)
