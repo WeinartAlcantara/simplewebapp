67a5e1240e587cc41b3a8a91b041b3ac9c37fa08
WeinartAlcantara <141305675+WeinartAlcantara@users.noreply.github.com>
2023-12-28 01:23:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 from flask import Flask, render_template
-import os
 import random
+
 
 app = Flask(__name__, template_folder="templates")
 
-# Generate a random color
 random_color = "#{:06x}".format(random.randint(0, 0xFFFFFF))
 
 color = random_color
 
-# List of possible messages
 messages = [
     "Canary",
     "Canary",
@@ -22,6 +20,7 @@ messages = [
 def index():
     random_message = random.choice(messages)
     return render_template("index.html", message=random_message, color=color)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5001)
